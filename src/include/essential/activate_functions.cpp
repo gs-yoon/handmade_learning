a9e71ad49b98c4c942923f5cd1d1fef7b934a949
gs-yoon <siljamjary7@khu.ac.kr>
2022-02-04 19:36:20 +0900

    env setup

---
+++ src/include/essential/activate_functions.cpp
@@ -0,0 +1,8 @@
+#include "activate_functions.h"
+#include <iostream>
+
+void print()
+{
+    int32 var = 0;
+    std::cout << var;
+}
---
+++ src/include/essential/activate_functions.h
@@ -0,0 +1,9 @@
+#ifndef __ACTIVATE_FUNCTIONS_H__
+#define __ACTIVATE_FUNCTIONS_H__
+
+#include "type_definition.h"
+
+
+void print();
+
+#endif
