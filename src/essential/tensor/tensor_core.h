f51fe9b9269111c9726f594e25be04c45d52015d
root <root@DESKTOP-PT8AQ7N.localdomain>
2022-03-13 23:53:53 +0900

    replace 5d-array with 1d-array

--- src/essential/tensor/tensor_core.h
+++ src/essential/tensor/tensor_core.h
@@ -5,8 +5,10 @@
 #include<math.h>
 #include<algorithm>
 #include<iostream>
+#include"cuda_utils.h"
 
 #define TENSORDEBUG 0
+#define CUDAENABLE 0
 #define DEFAULTMAXDIM 5
 #define ROWIDX 3
 #define COLIDX 4
@@ -41,21 +43,34 @@ long long g_make_cnt =0;
 class Tensor
 {
 private:
-    VALUETYPE***** root_ = nullptr;
+    VALUETYPE* root_ = nullptr;
 
     int* shape_ = nullptr;
     int row_ =0;
     int col_ =0;
     int rank_ = 0;
     int valid_ = 0;
+    int shape_0_1_2_3_4_ = 0; //total size
+    int shape_1_2_3_4_ = 0; 
+    int shape_2_3_4_ = 0; 
+    int shape_3_4_ = 0; 
+    int shape_4_ = 0; 
+    int size_ = 0;
 
 private:
-    VALUETYPE& root()const{ return root_[0][0][0][0][0]; }
-    VALUETYPE& root(int i)const{ return root_[0][0][0][0][i]; }
-    VALUETYPE& root(int i, int j)const{ return root_[0][0][0][i][j]; }
-    VALUETYPE& root(int i, int j, int k)const{ return root_[0][0][i][j][k]; }
-    VALUETYPE& root(int i, int j, int k, int l)const{ return root_[0][i][j][k][l]; }
-    VALUETYPE& root(int i, int j, int k, int l, int m)const{ return root_[i][j][k][l][m]; }
+    VALUETYPE& root()const{ return *root_; }
+    VALUETYPE& root(int i)const{ return *(root_ + i); }
+    VALUETYPE& root(int i, int j)const{ return *(root_ + (shape_4_ *i) + j); }
+    VALUETYPE& root(int i, int j, int k)const{ return *(root_ + (shape_3_4_ * i) + (shape_4_ *j) + k); }
+    VALUETYPE& root(int i, int j, int k, int l)const{ return *(root_ + (shape_2_3_4_ * i) + (shape_3_4_ * j) + (shape_4_ *k) + l);}
+    VALUETYPE& root(int i, int j, int k, int l, int m)const{ return *(root_ + (shape_1_2_3_4_*i) +  (shape_2_3_4_ * j) + (shape_3_4_ * k) + (shape_4_ *l) + m); }
+
+    VALUETYPE* rootAddress()const{ return root_; }
+    VALUETYPE* rootAddress(int i, int j, int k, int l, int m)const{ return (root_ + (shape_1_2_3_4_*i) +  (shape_2_3_4_ * j) + (shape_3_4_ * k) + (shape_4_ *l) + m); }
+
+
+    void breakTensorInGpu();
+    void allocInGpu();
 
     Tensor matMul1D(const Tensor& )const;
     Tensor matMul2D(const Tensor& )const;
@@ -95,11 +110,11 @@ public:
     int* getRawShape()const;
     int getShape(int idm)const;
     int setVal();
-    int setConstant(VALUETYPE);
+    void setConstant(VALUETYPE);
     int setRandom();
     int getSize()const;
     int rank()const;
-    VALUETYPE***** getData()const;
+    VALUETYPE* getData()const;
     Tensor matMul(const Tensor& )const;
     Tensor dotMul(const Tensor& )const;
     Tensor reshape(int);//const?
@@ -116,12 +131,16 @@ public:
     Tensor extract(int i, int j, int k, int l);
     Tensor extract(int i, int j, int k, int l, int m);
 
+    void copyHostToGpu();
+    void copyGpuToHost();
+
     VALUETYPE toScalar()
     {
         if( rank_ ==0 )
             return root(0);
         else
             printf("Invalid Converting. No Scalar\n");
+            return -1;
     }
     //Tensor partialCopy(int i, int j, int k, int l, Tesnor& x);
 
@@ -365,14 +384,14 @@ public:
     bool operator<(const Tensor t)
     {
         if (rank_ == 0)
-            return root(0) < t.root_[0][0][0][0][0]; 
+            return root(0) < t.root(0,0,0,0,0); 
         else
             return false;
     }
     bool operator>(const Tensor t)
     {
         if (rank_ == 0)
-            return root(0) > t.root_[0][0][0][0][0]; 
+            return root(0) > t.root(0,0,0,0,0); 
         else
             return false;
     }
@@ -414,13 +433,8 @@ Tensor::Tensor(const Tensor& cp)
         shape_[i] = cp.shape_[i];
     }
 
-    for(int d1_idx = 0 ; d1_idx < shape_[0] ; d1_idx++)
-        for( int d2_idx = 0; d2_idx < shape_[1] ; d2_idx++)
-            for( int d3_idx = 0; d3_idx < shape_[2] ; d3_idx++)
-                for( int d4_idx = 0; d4_idx < shape_[3] ; d4_idx++)
-                    std::copy(cp.root_[d1_idx][d2_idx][d3_idx][d4_idx], cp.root_[d1_idx][d2_idx][d3_idx][d4_idx] + shape_[4], root_[d1_idx][d2_idx][d3_idx][d4_idx]) ;
-                    //for( int d5_idx = 0; d5_idx < shape_[4] ; d5_idx++)
-                    //    root(d1_idx,d2_idx,d3_idx,d4_idx,d5_idx) = cp.root_[d1_idx][d2_idx][d3_idx][d4_idx][d5_idx] ;//copyc
+    std::copy(cp.root_, cp.root_ + size_, root_) ;
+
 }
 
 //assign constructor
@@ -442,6 +456,7 @@ Tensor& Tensor::operator=(const VALUETYPE scalar)
     }
 
     root(0) = scalar;
+    return *this;
 }
 
 //template<typename T>
@@ -462,6 +477,7 @@ Tensor& Tensor::operator=(const int scalar)
     }
 
     root(0) = scalar;
+    return *this;
 }
 //template<typename T>
 Tensor& Tensor::operator=(const bool scalar)
@@ -481,6 +497,7 @@ Tensor& Tensor::operator=(const bool scalar)
     }
 
     root(0) = scalar;
+    return *this;
 }
 //template<typename T>
 Tensor& Tensor::operator=(const Tensor& cp)
@@ -525,16 +542,7 @@ Tensor& Tensor::operator=(const Tensor& cp)
             shape_[i] = cp.shape_[i];
         }
     }
-    for(int d1_idx = 0 ; d1_idx < shape_[0] ; d1_idx++)
-        for( int d2_idx = 0; d2_idx < shape_[1] ; d2_idx++)
-            for( int d3_idx = 0; d3_idx < shape_[2] ; d3_idx++)
-                for( int d4_idx = 0; d4_idx < shape_[3] ; d4_idx++)
-                    std::copy(cp.root_[d1_idx][d2_idx][d3_idx][d4_idx], cp.root_[d1_idx][d2_idx][d3_idx][d4_idx] + shape_[4], root_[d1_idx][d2_idx][d3_idx][d4_idx]) ;
-                    //for( int d5_idx = 0; d5_idx < shape_[4] ; d5_idx++)
-                    //    root_[d1_idx][d2_idx][d3_idx][d4_idx][d5_idx] = cp.root_[d1_idx][d2_idx][d3_idx][d4_idx][d5_idx] ;
-    
-    //int tensor_size = shape_[0] *shape_[1] *shape_[2] *shape_[3] *shape_[4];
-    //std::copy(cp.root_, cp.root_ + tensor_size, root_);
+    std::copy(cp.root_, cp.root_ + size_, root_) ;
 
     return *this;
 }
@@ -547,51 +555,25 @@ inline void Tensor::makeTensor(int d1, int d2, int d3, int d4, int d5)
 
     valid_ = 1;
     g_make_cnt ++;
-    root_ = new VALUETYPE****[d1];
+
+    row_ = d4;
+    col_ = d5;
+
     shape_ = new int[DEFAULTMAXDIM];
-
-    for(int idx = 0 ; idx < d1 ; idx ++)
-    {
-        root_[idx] = new VALUETYPE***[d2];
-    }
-    
-    for(int ri = 0 ; ri < d1 ; ri++)
-    {
-        for( int ci = 0; ci < d2 ; ci++)
-        {
-            root_[ri][ci] = new VALUETYPE**[d3];
-        }
-    }
-
-    for(int ri = 0 ; ri < d1 ; ri++)
-    {
-        for( int ci = 0; ci < d2 ; ci++)
-        {
-            for( int di = 0; di < d3 ; di++)
-            {
-                root_[ri][ci][di] = new VALUETYPE*[d4];
-            }
-        }
-    }
-
-    for(int ri = 0 ; ri < d1 ; ri++)
-    {
-        for( int ci = 0; ci < d2 ; ci++)
-        {
-            for( int di = 0; di < d3 ; di++)
-            {
-                for(int d4i =0 ;d4i < d4 ; d4i++)
-                    root_[ri][ci][di][d4i] = new VALUETYPE[d5];
-            }
-        }
-    }
     shape_[0] = d1;
     shape_[1] = d2;
     shape_[2] = d3;
     shape_[3] = d4;
     shape_[4] = d5;
-    row_ = d4;
-    col_ = d5;
+
+    shape_4_ = shape_[4];
+    shape_3_4_= shape_[3] * shape_4_;
+    shape_2_3_4_ = shape_[2] * shape_3_4_;
+    shape_1_2_3_4_ = shape_[1] * shape_2_3_4_;
+    shape_0_1_2_3_4_ = shape_[0] * shape_1_2_3_4_;
+    size_ = shape_0_1_2_3_4_;
+    
+    root_ = new VALUETYPE[size_];
 
     if (d1 >1)
         rank_ = 5;
@@ -605,6 +587,7 @@ inline void Tensor::makeTensor(int d1, int d2, int d3, int d4, int d5)
         rank_ = 1;
     else
         rank_ = 0;
+
 }
 
 //template<typename T>
@@ -615,57 +598,26 @@ inline void Tensor::makeZeros(int d1, int d2, int d3, int d4, int d5)
 
     valid_ = 1;
     g_make_cnt ++;
-    root_ = new VALUETYPE****[d1];
+
+    row_ = d4;
+    col_ = d5;
+
     shape_ = new int[DEFAULTMAXDIM];
-
-    for(int idx = 0 ; idx < d1 ; idx ++)
-    {
-        root_[idx] = new VALUETYPE***[d2];
-    }
-    
-    for(int ri = 0 ; ri < d1 ; ri++)
-    {
-        for( int ci = 0; ci < d2 ; ci++)
-        {
-            root_[ri][ci] = new VALUETYPE**[d3];
-        }
-    }
-
-    for(int ri = 0 ; ri < d1 ; ri++)
-    {
-        for( int ci = 0; ci < d2 ; ci++)
-        {
-            for( int di = 0; di < d3 ; di++)
-            {
-                root_[ri][ci][di] = new VALUETYPE*[d4];
-            }
-        }
-    }
-
-    for(int ri = 0 ; ri < d1 ; ri++)
-    {
-        for( int ci = 0; ci < d2 ; ci++)
-        {
-            for( int di = 0; di < d3 ; di++)
-            {
-                for(int d4i =0 ;d4i < d4 ; d4i++)
-                {
-                    root_[ri][ci][di][d4i] = new VALUETYPE[d5];
-                    std::fill(root_[ri][ci][di][d4i], root_[ri][ci][di][d4i]+ d5, 0x00);
-                    //for(int d5_idx = 0 ; d5_idx < d5 ; d5_idx++)
-                     //   root_[ri][ci][di][d4i][d5_idx] = 0;
-                    //memset(root_[ri][ci][di][d4i], 0x00, sizeof(root_[ri][ci][di][d4i]));
-                }
-            }
-        }
-    }
     shape_[0] = d1;
     shape_[1] = d2;
     shape_[2] = d3;
     shape_[3] = d4;
     shape_[4] = d5;
-    row_ = d4;
-    col_ = d5;
+
+    shape_4_ = shape_[4];
+    shape_3_4_= shape_[3] * shape_4_;
+    shape_2_3_4_ = shape_[2] * shape_3_4_;
+    shape_1_2_3_4_ = shape_[1] * shape_2_3_4_;
+    shape_0_1_2_3_4_ = shape_[0] * shape_1_2_3_4_;
+    size_ = shape_0_1_2_3_4_;
+    
+    root_ = new VALUETYPE[size_];
+    std::fill(root_, root_ + size_, 0x00);
 
     if (d1 >1)
         rank_ = 5;
@@ -679,6 +631,7 @@ inline void Tensor::makeZeros(int d1, int d2, int d3, int d4, int d5)
         rank_ = 1;
     else
         rank_ = 0;
+
 }
 
 //template<typename T>
@@ -687,39 +640,6 @@ void Tensor::breakTensor()
     g_delete_cnt ++;
     valid_ = 0;
 
-    for(int d1_idx = 0 ; d1_idx < shape_[0] ; d1_idx++)
-    {
-        for( int d2_idx = 0; d2_idx < shape_[1] ; d2_idx++)
-        {
-            for( int d3_idx = 0; d3_idx < shape_[2] ; d3_idx++)
-            {
-                for( int d4_idx = 0; d4_idx < shape_[3] ; d4_idx++)
-                {
-                    if (root_[d1_idx][d2_idx][d3_idx][d4_idx] != nullptr)
-                    {
-                        delete[] root_[d1_idx][d2_idx][d3_idx][d4_idx];
-                        root_[d1_idx][d2_idx][d3_idx][d4_idx] = nullptr;
-                    }
-
-                }
-                if (root_[d1_idx][d2_idx][d3_idx] != nullptr)
-                {
-                    delete[] root_[d1_idx][d2_idx][d3_idx];
-                    root_[d1_idx][d2_idx][d3_idx] = nullptr;
-                }
-            }
-            if (root_[d1_idx][d2_idx] != nullptr)
-            {
-                delete[] root_[d1_idx][d2_idx];
-                root_[d1_idx][d2_idx] = nullptr;
-            }
-        }
-        if (root_[d1_idx] != nullptr)
-        {
-            delete[] root_[d1_idx];
-            root_[d1_idx] = nullptr;
-        }
-    }
     if (root_ != nullptr)
         {
             delete[] root_;
@@ -732,6 +652,7 @@ void Tensor::breakTensor()
         shape_ = nullptr;
     }
 }
+
 //template<typename T>
 int* Tensor::getRawShape() const
 {
@@ -767,21 +688,15 @@ int Tensor::getShape(int dim) const
 
 int Tensor::setVal()
 {
-
+    return 0;
 }
-int Tensor::setConstant(VALUETYPE scalar)
-{
-    
-    for(int d1_idx = 0 ; d1_idx < shape_[0]; d1_idx++)
-        for( int d2_idx = 0; d2_idx < shape_[1] ;d2_idx++)
-            for( int d3_idx = 0; d3_idx < shape_[2] ;d3_idx++)
-                for( int d4_idx = 0; d4_idx < shape_[3] ; d4_idx++)
-                    for( int d5_idx = 0; d5_idx < shape_[4] ;d5_idx++)
-                        root_[d1_idx][d2_idx][d3_idx][d4_idx][d5_idx] = scalar;
+void Tensor::setConstant(VALUETYPE scalar)
+{  
+    std::fill(root_, root_ + size_, scalar);
 }
 int Tensor::setRandom()
 {
-
+    return 0;
 }
 int Tensor::getSize()const
 {
@@ -796,7 +711,7 @@ int Tensor::rank() const
     return rank_;
 }
 //template<typename T>
-VALUETYPE***** Tensor::getData() const
+VALUETYPE* Tensor::getData() const
 {
 
     return root_;
@@ -808,7 +723,7 @@ Tensor Tensor::matMul1D(const Tensor& in_tensor) const
     Tensor result;
     if ( (rank_ == 0 ) && (in_tensor.rank_ > 0) )
     {
-        VALUETYPE* ptr = in_tensor.root_[0][0][0][0];
+        VALUETYPE* ptr = in_tensor.rootAddress(0,0,0,0,0);
         int numOfElements = 1;
         for(int i =0 ; i< in_tensor.rank_ ; i++)
         {
@@ -822,7 +737,7 @@ Tensor Tensor::matMul1D(const Tensor& in_tensor) const
     }
     else if ( (in_tensor.rank_ == 0 ) && (rank_ > 0 )  )
     {
-        VALUETYPE* ptr = root_[0][0][0][0];
+        VALUETYPE* ptr = rootAddress(0,0,0,0,0);
         int numOfElements = 1;
         for(int i =0 ; i< rank_ ; i++)
         {
@@ -838,6 +753,7 @@ Tensor Tensor::matMul1D(const Tensor& in_tensor) const
     {
         printf("Dimension erorr. Use matMul2D or matMul3D \n") ;
     }
+    return result;
 }
 
 //template<typename T>
@@ -1712,8 +1628,7 @@ Tensor Tensor::extract(int i, int j)
     if (j == -1)
     {
         result.makeTensor(1,1,1,1,shape_[4]);
-        for(int d4 =0; d4 < shape_[4]; d4++)
-            result.root_[0][0][0][0][d4] = root_[0][0][0][i][d4]; 
+        std::copy(rootAddress(0,0,0,i,0),rootAddress(0,0,0,i,0) + shape_[4], result.rootAddress(0,0,0,0,0) );
     }
     return result;
 }
@@ -1726,16 +1641,13 @@ Tensor Tensor::extract(int i, int j, int k)
         if (j == -1)
         {
             result.makeTensor(1,1,1,shape_[3],shape_[4]);
-            for(int d3 =0; d3 < shape_[3]; d3++)
-                for(int d4 =0; d4 < shape_[4]; d4++)
-                    result.root_[0][0][0][d3][d4] = root_[0][0][i][d3][d4]; 
+            std::copy(rootAddress(0,0,i,0,0),rootAddress(0,0,i,0,0) + shape_3_4_, result.rootAddress(0,0,0,0,0) );
 
         }
         else
         {
             result.makeTensor(1,1,1,1,shape_[4]);
-            for(int d4 =0; d4 < shape_[4]; d4++)
-                result.root_[0][0][0][0][d4] = root_[0][0][i][j][d4];   
+            std::copy(rootAddress(0,0,i,j,0),rootAddress(0,0,i,j,0) + shape_4_, result.rootAddress(0,0,0,0,0) );
         }
     }
     else
@@ -1754,24 +1666,18 @@ Tensor Tensor::extract(int i, int j, int k, int l)
             if (j == -1)
             {
                 result.makeTensor(1,1,shape_[2],shape_[3],shape_[4]);
-                for(int d2 =0; d2 < shape_[2]; d2++)
-                    for(int d3 =0; d3 < shape_[3]; d3++)
-                        for(int d4 =0; d4 < shape_[4]; d4++)
-                            result.root_[0][0][d2][d3][d4] = root_[0][i][d2][d3][d4]; 
+                std::copy(rootAddress(0,i,0,0,0),rootAddress(0,i,0,0,0) + shape_2_3_4_, result.rootAddress(0,0,0,0,0) );
             }
             else
             {
                 result.makeTensor(1,1,1,shape_[3],shape_[4]);
-                for(int d3 =0; d3 < shape_[3]; d3++)
-                    for(int d4 =0; d4 < shape_[4]; d4++)
-                        result.root_[0][0][0][d3][d4] = root_[0][i][j][d3][d4]; 
+                std::copy(rootAddress(0,i,j,0,0),rootAddress(0,i,j,0,0) + shape_3_4_, result.rootAddress(0,0,0,0,0) );
             }
         }
         else
         {
             result.makeTensor(1,1,1,shape_[3],shape_[4]);
-            for(int d4 =0; d4 < shape_[4]; d4++)
-                result.root_[0][0][0][0][d4] = root_[0][i][j][k][d4]; 
+            std::copy(rootAddress(0,i,j,k,0),rootAddress(0,i,j,k,0) + shape_4_, result.rootAddress(0,0,0,0,0) );
         }
         
     }
@@ -1789,34 +1695,24 @@ Tensor Tensor::extract(int i, int j, int k, int l, int m)
                 if (j == -1)
                 {
                     result.makeTensor(1,shape_[1],shape_[2],shape_[3],shape_[4]);
-                    for(int d1 =0; d1 < shape_[1]; d1++)
-                        for(int d2 =0; d2 < shape_[2]; d2++)
-                            for(int d3 =0; d3 < shape_[3]; d3++)
-                                for(int d4 =0; d4 < shape_[4]; d4++)
-                                    result.root_[0][d1][d2][d3][d4] = root_[i][d1][d2][d3][d4];
+                    std::copy(rootAddress(i,0,0,0,0),rootAddress(i,0,0,0,0) + shape_1_2_3_4_, result.rootAddress(0,0,0,0,0) );
                 }
                 else
                 {
                     result.makeTensor(1,1,shape_[2],shape_[3],shape_[4]);
-                    for(int d2 =0; d2 < shape_[2]; d2++)
-                        for(int d3 =0; d3 < shape_[3]; d3++)
-                            for(int d4 =0; d4 < shape_[4]; d4++)
-                                result.root_[0][0][d2][d3][d4] = root_[i][j][d2][d3][d4];
+                    std::copy(rootAddress(i,j,0,0,0),rootAddress(i,j,0,0,0) + shape_2_3_4_, result.rootAddress(0,0,0,0,0) );
                 }
             }
             else
             {
                 result.makeTensor(1,1,shape_[2],shape_[3],shape_[4]);
-                for(int d3 =0; d3 < shape_[3]; d3++)
-                    for(int d4 =0; d4 < shape_[4]; d4++)
-                        result.root_[0][0][0][d3][d4] = root_[i][j][k][d3][d4];
+                std::copy(rootAddress(i,j,k,0,0),rootAddress(i,j,k,0,0) + shape_3_4_, result.rootAddress(0,0,0,0,0) );
             }
         }
         else
         {
             result.makeTensor(1,1,shape_[2],shape_[3],shape_[4]);
-            for(int d4 =0; d4 < shape_[4]; d4++)
-                result.root_[0][0][0][0][d4] = root_[i][j][k][l][d4];
+            std::copy(rootAddress(i,j,k,l,0),rootAddress(i,j,k,l,0) + shape_4_, result.rootAddress(0,0,0,0,0) );
         }
     }
     else{
